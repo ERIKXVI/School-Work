3fa1edd264cffc17983a560e500dc3d45ce32c14
ERIKXVI <Erik.fastesson@gmail.com>
2023-04-26 11:17:21 +0200

    idk

---
+++ template/main.cpp
@@ -0,0 +1,93 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // Register the window class
+    const char CLASS_NAME[] = "My Window Class";
+
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles
+        CLASS_NAME,                     // Window class
+        "My App",                       // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    // Show the window
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop
+    MSG msg = {};
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+    {
+        // Create a button
+        HWND button = CreateWindow(
+            "BUTTON",
+            "Click me",
+            WS_VISIBLE | WS_CHILD,
+            10, 10, 100, 30,
+            hwnd,
+            NULL,
+            NULL,
+            NULL
+        );
+        break;
+    }
+
+    case WM_COMMAND:
+    {
+        // Handle button click
+        if (LOWORD(wParam) == BN_CLICKED)
+        {
+            MessageBox(hwnd, "Button clicked!", "Message", MB_OK);
+        }
+        break;
+    }
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
