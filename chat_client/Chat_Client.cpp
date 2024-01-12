adefcaf13bd658cf4ca03342e1302f9af18ca220
ERIKXVI <Erik.fastesson@gmail.com>
2024-01-12 10:36:11 +0100

    Delete unnecessary build files

---
+++ chat_client/Chat_Client.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void showLoadingScreen()
+{
+	cout << "Loading..." << endl;
+	cout << "[";
+	for (int i = 0; i <= 10; i++)
+	{
+		cout << "=";
+		cout.flush();
+		this_thread::sleep_for(chrono::seconds(1));
+	}
+	cout << "]" << endl;
+}
+
+int main()
+{
+	cout << "Hello and welcome to sexy chat!" << endl;
+	cout << "-----------------------------" << endl;
+	cout << "Please enter your Username: ";
+	string username;
+	cin >> username;
+
+	showLoadingScreen();
+
+	cout << "Welcome " << username << " to sexy chat!" << endl;
+	cout << "--------------------------------------" << endl;
+	cin.ignore();
+	cin.get();
+	return 0;
+}
