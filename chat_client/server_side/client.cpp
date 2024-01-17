65596a0051c8a8ce7f87f6aa1d64f5437076123f
ERIKXVI <98014388+ERIKXVI@users.noreply.github.com>
2024-01-17 17:23:13 +0100

    Delete unnecessary files and libraries

---
+++ chat_client/server_side/client.cpp
@@ -0,0 +1,147 @@
+#include <windows.h>
+#include <cwchar>
+
+#pragma comment(lib, "ws2_32.lib") // Add this line
+
+#define ID_USERNAME 101
+#define ID_SAFE_CHAT 102
+#define ID_UNSAFE_CHAT 103
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    static HWND hwndChat, hwndInput, hwndUsername, hwndSafeChat, hwndUnsafeChat;
+
+    switch (uMsg)
+    {
+    case WM_CREATE:
+        hwndUsername = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"", WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL, 10, 10, 200, 25, hwnd, (HMENU)ID_USERNAME, NULL, NULL);
+        hwndSafeChat = CreateWindow(L"BUTTON", L"Join Safe Chat", WS_VISIBLE | WS_CHILD, 220, 10, 150, 25, hwnd, (HMENU)ID_SAFE_CHAT, NULL, NULL);
+        hwndUnsafeChat = CreateWindow(L"BUTTON", L"Join Unsafe Chat", WS_VISIBLE | WS_CHILD, 380, 10, 150, 25, hwnd, (HMENU)ID_UNSAFE_CHAT, NULL, NULL);
+        hwndChat = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_READONLY, 0, 50, 200, 100, hwnd, (HMENU)1, NULL, NULL);
+        hwndInput = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL, 0, 150, 200, 50, hwnd, (HMENU)2, NULL, NULL);
+        CreateWindow(L"BUTTON", L"Send", WS_VISIBLE | WS_CHILD, 0, 200, 200, 25, hwnd, (HMENU)3, NULL, NULL);
+        break;
+    case WM_SIZE:
+        MoveWindow(hwndChat, 0, 50, LOWORD(lParam), HIWORD(lParam) - 125, TRUE);
+        MoveWindow(hwndInput, 0, HIWORD(lParam) - 75, LOWORD(lParam), 50, TRUE);
+        MoveWindow(GetDlgItem(hwnd, 3), 0, HIWORD(lParam) - 25, LOWORD(lParam), 25, TRUE);
+        break;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == 3)
+        {
+            int len = GetWindowTextLength(hwndInput) + 1;
+            wchar_t* text = new wchar_t[len];
+            GetWindowText(hwndInput, text, len);
+
+            wchar_t username[256];
+            GetWindowText(hwndUsername, username, 256);
+
+            wchar_t message[512];
+            swprintf(message, 512, L"%s: %s", username, text);
+
+            // Here you can send the message to the server
+            // ...
+
+            delete[] text;
+        }
+        else if (LOWORD(wParam) == ID_SAFE_CHAT)
+        {
+            // Join safe server
+            int len = GetWindowTextLength(hwndUsername) + 1;
+            wchar_t* username = new wchar_t[len];
+            GetWindowText(hwndUsername, username, len);
+
+            // Initialize Winsock
+            WSADATA wsaData;
+            int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+            if (iResult != 0) {
+                MessageBox(NULL, L"WSAStartup failed", L"Error", MB_ICONEXCLAMATION | MB_OK);
+                delete[] username;
+                break;
+            }
+
+            // Create a socket
+            SOCKET ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+            if (ConnectSocket == INVALID_SOCKET) {
+                MessageBox(NULL, L"Error at socket", L"Error", MB_ICONEXCLAMATION | MB_OK);
+                WSACleanup();
+                delete[] username;
+                break;
+            }
+
+            // Connect to the server
+            sockaddr_in clientService;
+            clientService.sin_family = AF_INET;
+            clientService.sin_addr.s_addr = inet_addr("0.0.0.0"); // Replace with your server's IP address
+            clientService.sin_port = htons(12345); // Replace with your server's port number
+
+            if (connect(ConnectSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {
+                MessageBox(NULL, L"Failed to connect", L"Error", MB_ICONEXCLAMATION | MB_OK);
+                closesocket(ConnectSocket);
+                WSACleanup();
+                delete[] username;
+                break;
+            }
+
+            // Send the username to the server
+            char buffer[512];
+            size_t convertedChars = 0;
+            wcstombs_s(&convertedChars, buffer, 512, username, _TRUNCATE);
+            send(ConnectSocket, buffer, static_cast<int>(strlen(buffer)), 0);
+
+            // Clean up
+            delete[] username;
+        }
+
+        else if (LOWORD(wParam) == ID_UNSAFE_CHAT)
+        {
+            // Join unverified server
+            wchar_t ip[16];
+            GetWindowText(hwndInput, ip, 16);
+            // Connect to the server at the entered IP address
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    const wchar_t CLASS_NAME[] = L"ChatClient";
+
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    if (!RegisterClass(&wc))
+    {
+        MessageBox(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"Chat Client", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1000, 700, NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL)
+    {
+        MessageBox(NULL, L"Window Creation Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
---
+++ chat_client/server_side/main.cpp
@@ -0,0 +1,227 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <deque>
+#include <list>
+#include <set>
+#include <memory>
+
+using boost::asio::ip::tcp;
+
+typedef std::deque<std::string> chat_message_queue;
+
+class chat_participant
+{
+public:
+    virtual ~chat_participant() {}
+    virtual void deliver(const std::string& msg) = 0;
+    std::string username;
+};
+
+typedef std::shared_ptr<chat_participant> chat_participant_ptr;
+
+class chat_room
+{
+public:
+    void join(chat_participant_ptr participant)
+    {
+        participants_.insert(participant);
+        std::for_each(recent_msgs_.begin(), recent_msgs_.end(),
+            boost::bind(&chat_participant::deliver, participant, _1));
+        print_participants();
+    }
+
+    void leave(chat_participant_ptr participant)
+    {
+        participants_.erase(participant);
+        print_participants();
+    }
+
+    void deliver(const std::string& msg)
+    {
+        recent_msgs_.push_back(msg);
+        while (recent_msgs_.size() > max_recent_msgs)
+            recent_msgs_.pop_front();
+
+        std::for_each(participants_.begin(), participants_.end(),
+            boost::bind(&chat_participant::deliver, _1, boost::ref(msg)));
+    }
+
+    void print_participants()
+    {
+        std::cout << "Connected users:\n";
+        for (const auto& participant : participants_)
+        {
+            std::cout << participant->username << "\n";
+        }
+    }
+
+private:
+    std::set<chat_participant_ptr> participants_;
+    enum { max_recent_msgs = 100 };
+    chat_message_queue recent_msgs_;
+};
+
+class chat_session : public chat_participant, public std::enable_shared_from_this<chat_session>
+{
+public:
+    chat_session(boost::asio::io_service& io_service, chat_room& room)
+        : socket_(io_service),
+        room_(room)
+    {
+    }
+
+    tcp::socket& socket()
+    {
+        return socket_;
+    }
+
+    void start()
+    {
+        room_.join(shared_from_this());
+        boost::asio::async_read_until(socket_, input_buffer_, "\n",
+            boost::bind(&chat_session::handle_read, shared_from_this(),
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+    }
+
+    void deliver(const std::string& msg)
+    {
+        bool write_in_progress = !message_queue_.empty();
+        message_queue_.push_back(msg);
+        if (!write_in_progress)
+        {
+            boost::asio::async_write(socket_,
+                boost::asio::buffer(message_queue_.front() + "\n"),
+                boost::bind(&chat_session::handle_write, shared_from_this(),
+                    boost::asio::placeholders::error));
+        }
+    }
+
+    void handle_read(const boost::system::error_code& error, size_t bytes_transferred)
+    {
+        if (!error)
+        {
+            std::string msg;
+            std::istream is(&input_buffer_);
+            std::getline(is, msg);
+            if (username.empty())
+            {
+                username = msg;
+            }
+            else
+            {
+                room_.deliver(msg);
+            }
+            boost::asio::async_read_until(socket_, input_buffer_, "\n",
+                boost::bind(&chat_session::handle_read, shared_from_this(),
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred));
+        }
+        else
+        {
+            room_.leave(shared_from_this());
+        }
+    }
+
+    void handle_write(const boost::system::error_code& error)
+    {
+        if (!error)
+        {
+            message_queue_.pop_front();
+            if (!message_queue_.empty())
+            {
+                boost::asio::async_write(socket_,
+                    boost::asio::buffer(message_queue_.front() + "\n"),
+                    boost::bind(&chat_session::handle_write, shared_from_this(),
+                        boost::asio::placeholders::error));
+            }
+        }
+        else
+        {
+            room_.leave(shared_from_this());
+        }
+    }
+
+private:
+    tcp::socket socket_;
+    chat_room& room_;
+    boost::asio::streambuf input_buffer_;
+    chat_message_queue message_queue_;
+};
+
+typedef boost::shared_ptr<chat_session> chat_session_ptr;
+
+class chat_server
+{
+public:
+    chat_server(boost::asio::io_service& io_service,
+        const tcp::endpoint& endpoint)
+        : io_service_(io_service),
+        acceptor_(io_service, endpoint)
+    {
+        start_accept();
+    }
+
+    void start_accept()
+    {
+        chat_session_ptr new_session(new chat_session(io_service_, room_));
+        acceptor_.async_accept(new_session->socket(),
+            boost::bind(&chat_server::handle_accept, this, new_session,
+                boost::asio::placeholders::error));
+    }
+
+    void handle_accept(chat_session_ptr session,
+        const boost::system::error_code& error)
+    {
+        if (!error)
+        {
+            boost::asio::post(io_service_,
+                boost::bind(&chat_session::start, session));
+            start_accept();
+        }
+    }
+
+    void start_session(chat_session_ptr session)
+    {
+        session->start();
+        start_accept();
+    }
+
+private:
+    boost::asio::io_service& io_service_;
+    tcp::acceptor acceptor_;
+    chat_room room_;
+};
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc < 2)
+        {
+            std::cerr << "Usage: chat_server <port> [<port> ...]\n";
+            return 1;
+        }
+
+        boost::asio::io_service io_service;
+        boost::asio::io_service::work work(io_service);  // Add this line
+
+        std::list<chat_server> servers;
+        for (int i = 1; i < argc; ++i)
+        {
+            tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[i]));
+            servers.emplace_back(io_service, endpoint);
+            std::cout << "Server is running on " << endpoint << "\n";
+        }
+
+        io_service.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
